226c6fce20b3cb870e2a8eb9b6411eb0224da7d5
raul <raulmejianarea@gmail.com>
2023-08-07 18:51:58 +0200

    Initial Commit

---
+++ JARVIS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JARVIS
+//
+//  Created by raul Mejia on 7/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JARVIS/JARVISApp.swift
@@ -0,0 +1,17 @@
+//
+//  JARVISApp.swift
+//  JARVIS
+//
+//  Created by raul Mejia on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct JARVISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
